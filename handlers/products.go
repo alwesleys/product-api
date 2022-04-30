62725ee19b20d2bdd7af492ef0d847e7c5ff3905
Wes <alwesley.salac@gmail.com>
2022-04-30 20:53:39 +0800

    added validation function in middleware

--- handlers/products.go
+++ handlers/products.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -99,6 +100,15 @@ func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
 		err := prod.FromJSON(r.Body)
 		if err != nil {
 			http.Error(rw, "Unable to decode json", http.StatusBadRequest)
+			return
+		}
+
+		// validate product
+		if err := prod.Validate(); err != nil {
+			http.Error(
+				rw,
+				fmt.Sprintf("Error reading product: %s", err),
+				http.StatusBadRequest)
 			return
 		}
 
